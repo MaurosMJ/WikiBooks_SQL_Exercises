9ca9e19399be4d6d60a033afdbfcad1701e32ae2
Mauros Milach Junior <mauros99milach@gmail.com>
2024-10-22 11:13:47 -0300

    feat: Solution.sql

--- 1. The computer store/Solution.sql
+++ 1. The computer store/Solution.sql
@@ -22,3 +22,6 @@ INSERT INTO Manufacturers(Code,Name) VALUES (1,'Sony'),(2,'Creative Labs'),(3,'H
 INSERT INTO Products(Code,Name,Price,Manufacturer) VALUES(1,'Hard drive',240,5),(2,'Memory',120,6),(3,'ZIP drive',150,4),(4,'Floppy disk',5,6),(5,'Monitor',240,1),
 (6,'DVD drive',180,2),(7,'CD drive',90,2),(8,'Printer',270,3),(9,'Toner cartridge',66,3),(10,'DVD burner',180,2);
 commit;
+
+-- 1.Select the names of all the products in the store.
+select name from products;
